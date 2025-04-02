8ee7382c79932b6e5432559234c063945b261706
Silvino Padilha Silva Nogueira <silvinopsn@dinfor.uniceub>
2025-04-02 19:33:18 -0300

    arquivo professor

--- compiler.h
+++ compiler.h
@@ -1,14 +1,11 @@
 #ifndef COMPILER_H
 #define COMPILER_H
 
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
-#include <stdbool.h>
 
-enum {
-    COMPILER_FILE_COMPILED_OK,
-    COMPILER_FAILED_WITH_ERRORS
-};
+/* BEGIN - LAB 2 ---------------------------------*/
+#include <stdbool.h>
 
 struct pos {
     int line;
@@ -16,39 +13,39 @@ struct pos {
     const char* filename;
 };
 
-enum {
-    LEXICAL_ANALYSIS_ALL_OK,
-    LEXICAL_ANALYSIS_INPUT_ERROR
+#define NUMERIC_CASE \
+    case '0':   \
+    case '1':   \
+    case '2':   \
+    case '3':   \
+    case '4':   \
+    case '5':   \
+    case '6':   \
+    case '7':   \
+    case '8':   \
+    case '9'
+
+    enum {
+    LEXICAL_ANALYSIS_ALL_OK ,
+    LEXICAL_ANALYSIS_IMPUT_ERROR
 };
 
 enum {
-    TOKEN_TYPE_KEYWORD,
-    TOKEN_TYPE_IDENTIFIER,
-    TOKEN_TYPE_OPERATOR,
-    TOKEN_TYPE_SYMBOL,
-    TOKEN_TYPE_NUMBER,
-    TOKEN_TYPE_STRING,
-    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_KEYWORD ,
+    TOKEN_TYPE_IDENTIFIER ,
+    TOKEN_TYPE_OPERATOR ,
+    TOKEN_TYPE_SYMBOL ,
+    TOKEN_TYPE_NUMBER ,
+    TOKEN_TYPE_STRING ,
+    TOKEN_TYPE_COMMENT ,
     TOKEN_TYPE_NEWLINE
 };
 
-struct compile_process {
-    // Como o arquivo deve ser compilado
-    int flags;
-
-    struct pos pos;
-
-    struct compile_process_input_file {
-        FILE* fp;
-        const char* abs_path;
-    } cfile;
-
-    FILE* ofile;
-};
-
-struct token{
+struct token{ 
     int type;
     int flags;
+
+    struct pos pos; //Identificar onde o token esta no arquivo.
 
     union {
         char cval;
@@ -57,8 +54,8 @@ struct token{
         unsigned long lnum;
         unsigned long long llnum;
         void* any;
-    };
 
+    };
     // Sera 'true' se tiver um espaço entre um token e o próximo token.
     bool whitespace;
 
@@ -66,35 +63,32 @@ struct token{
     const char* between_brackets;
 };
 
+struct lex_process;
+
+// Definicao de ponteiros para funcoes.
+typedef char (*LEX_PROCESS_NEXT_CHAR) (struct lex_process* process);            
+typedef char (*LEX_PROCESS_PEEK_CHAR) (struct lex_process* process);            
+typedef void (*LEX_PROCESS_PUSH_CHAR) (struct lex_process* process, char c);
+
 struct lex_process_functions {
     LEX_PROCESS_NEXT_CHAR next_char;
     LEX_PROCESS_PEEK_CHAR peek_char;
     LEX_PROCESS_PUSH_CHAR push_char;
 };
 
-struct lex_process;
-
-// Definição de ponteiros para funções.
-typedef char (*LEX_PROCESS_NEXT_CHAR) (struct lex_process* process);
-typedef char (*LEX_PROCESS_PEEK_CHAR) (struct lex_process* process);
-typedef void (*LEX_PROCESS_PUSH_CHAR) (struct lex_process* process, char c);
-
 struct lex_process {
     struct pos pos;
     struct vector* token_vec;
     struct compile_process* compiler;
 
-    int current_expression_count; // Qts parênteses existem no momento.
+    
+    int current_expression_count; //Qts parenteses existem no momento.
 
     struct buffer* parentheses_buffer;
     struct lex_process_functions* function;
 
-    void* private; // Dados privados que o lexer não entende mas o programador entende.
+    void* private; //Dados privados que o lexer nao entende mas o programador entende.
 };
-
-
-int compile_file(const char* filename, const char* out_filename, int flags);
-struct compile_process* compile_process_create(const char* filename, const char* filename_out, int flags);
 
 // Funcoes do arquivo cprocess.c
 char compile_process_next_char(struct lex_process* lex_process);
@@ -109,6 +103,39 @@ struct vector* lex_process_tokens(struct lex_process* process);
 
 // Funcoes do arquivo lexer.c
 int lex(struct lex_process* process);
+
+// Funcoes do arquivo compiler.c
+void compiler_error(struct compile_process* compiler, const char* msg, ...);
+void compiler_warning(struct compile_process* compiler, const char* msg, ...);
+
+
+/* END - LAB 2 ---------------------------------*/
+
+enum{
+    COMPILER_FILE_COMPILED_OK,
+    COMPILER_FAILED_WITH_ERRORS
+};
+
+struct compile_process {
+    // Como o arquivo deve ser compilado
+    int flags;
+
+    /* LAB2*/
+    struct pos pos;
+
+    struct compile_process_imput_file{
+        FILE* fp;
+        const char* abs_path;
+    } cfile;
+
+    FILE* ofile;
+};
+
+
+int compile_file(const char* filename, const char* out_finename, int flags);
+struct compile_process* compile_process_create(const char* filename, const char* filename_out, int flags);
+
+
 
 
 #endif
